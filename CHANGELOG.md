5fd8c5513f99fe30f414982295e03e14e0748117
Chris Luke <chrisy@flirble.org>
2014-10-23 14:44:19 -0400

    update changelog, fix some license headers

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+RTOS Reference Implementation Change Log
+========================================
+
+
+Version 0.1
+-----------
+
+* Wire up FreeRTOS.
+* Wire up serial peripheral support.
+* Wire up newlib requirements.
+* Put together basic POSIX I/O support.
+
--- src/main.h
+++ src/main.h
@@ -1,3 +1,12 @@
+/**
+ * vim: set softtabstop=4 shiftwidth=4 tabstop=4 expandtab:
+ * \file src/main.h
+ *
+ * This file is distributed under the terms of the MIT License.
+ * See the LICENSE file at the top of this tree, or if it is missing a copy can
+ * be found at http://opensource.org/licenses/MIT
+ */
+
 #ifndef  __MAIN_H
 #define  __MAIN_H
 
